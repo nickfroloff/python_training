08f78bbfebd0e242c4cdfb24e8763022c9de6467
nickfroloff <107642484+nickfroloff@users.noreply.github.com>
2022-09-15 13:43:30 +0300

    Add files via upload

--- task#3-3.py
+++ task#3-3.py
@@ -4,13 +4,14 @@
 
 # Пример:
 # - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
+#****************************************************************
 
 list = [1.1, 1.2, 3.1, 5, 10.01]
-print(list)
+print('список из вещественных чисел', list)
 
 def dif(list):
     dif_max_min =[]
     for i in range(len(list)):
         dif_max_min.append(list[i]%1)
     return max(dif_max_min) - min(dif_max_min)
-print(round(dif(list),2))
+print('разницу между максимальным и минимальным значением дробной части элементов =', round(dif(list),2))
