f9e1cba537bfd8aacee41b59e8ec9cbf6edca849
nickfroloff <107642484+nickfroloff@users.noreply.github.com>
2022-09-26 11:10:04 +0300

    Add files via upload

--- task#6-5.py
+++ task#6-5.py
@@ -1,11 +1,19 @@
-# 6-Сформировать список из N членов последовательности.
-# Для N = 5: 1, -3, 9, -27, 81 и т.д.
-#*******************************************
+# 5- Найти произведение пар чисел в списке. 
+# Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+#*****************************************************************************
 
-import math
+
+from random import randint
 def get_number(input_string, start=None, stop=None) -> int:
             num = int(input(input_string))
             return num
-num = get_number("Введите количество членов последовательности: ",1)
-array = [int(math.pow((-1), i)*3**i) for i in range(num)]
-print(f'\nСписок из {num} членов последовательности: {array}\n')
+
+num = get_number("Введите количество символов в массиве: ",1)
+min_n = 0
+max_n = 20
+
+list_numbers = [randint (min_n,max_n) for _ in range(num)]
+new_list = [list_numbers[i]*list_numbers[num-1-i] for i in range(num//2)] 
+if num%2 != 0: 
+    new_list.append(list_numbers[num-1-int(num/2)]**2)
+print(f'\nИз списка: {list_numbers} получается список: {new_list}\n')
